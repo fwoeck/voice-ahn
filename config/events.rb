3ca028d943960c396b7db0b9797609cf7eeb342c
Frank Woeckener <fwoeck@gmail.com>
2014-06-18 05:41:13 +0200

    add ruby version/gemset

--- config/events.rb
+++ config/events.rb
@@ -2,17 +2,11 @@
 
 Adhearsion::Events.draw do
 
-  # Register global handlers for events
-  #
-  # eg. Handling Punchblock events
   # punchblock do |event|
-  #   ...
+  #   puts event
   # end
-  #
-  # eg Handling PeerStatus AMI events
-  # ami :name => 'PeerStatus' do |event|
-  #   ...
-  # end
-  #
 
+  # ami name: 'PeerStatus' do |event|
+  #   puts event
+  # end
 end
