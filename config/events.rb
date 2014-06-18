195a6ef68116250b08f4ea3be9ffa1650644b12d
Frank Woeckener <fwoeck@gmail.com>
2014-06-18 21:38:55 +0200

    cleanup fanout publishing

--- config/events.rb
+++ config/events.rb
@@ -3,6 +3,7 @@
 Adhearsion::Events.draw do
 
   punchblock do |event|
+    AMQPManager.push_publish(event.to_json)
     AMQPManager.ahn_publish(event.to_json, routing_key: 'voice.ahn')
   end
 
--- lib/amqp.rb
+++ lib/amqp.rb
@@ -1,16 +1,30 @@
 module AMQPManager
 
-  def self.channel
-    Thread.current[:channel] ||= @connection.create_channel
+  def self.ahn_channel
+    Thread.current[:ahn_channel] ||= @connection.create_channel
   end
 
-  def self.xchange
-    Thread.current[:xchange] ||= channel.topic('voice.ahn', auto_delete: false)
+  def self.ahn_xchange
+    Thread.current[:ahn_xchange] ||= ahn_channel.topic('voice.ahn', auto_delete: false)
   end
 
   def self.ahn_publish(*args)
-    xchange.publish(*args)
+    ahn_xchange.publish(*args)
   end
+
+
+  def self.push_channel
+    Thread.current[:push_channel] ||= @connection.create_channel
+  end
+
+  def self.push_xchange
+    Thread.current[:push_xchange] ||= push_channel.fanout('voice.push', auto_delete: false)
+  end
+
+  def self.push_publish(*args)
+    push_xchange.publish(*args)
+  end
+
 
   def self.shutdown
     @connection.close
@@ -24,7 +38,7 @@ module AMQPManager
     )
     @connection.start
 
-    channel.queue('voice.ahn', auto_delete: false)
-           .bind(xchange, routing_key: 'voice.ahn')
+    ahn_channel.queue('voice.ahn', auto_delete: false)
+               .bind(ahn_xchange, routing_key: 'voice.ahn')
   end
 end
