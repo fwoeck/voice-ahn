05cc4b9f0ed148da5088d6cc00a9948df0de9ec0
Frank Woeckener <fwoeck@gmail.com>
2014-06-28 13:53:35 +0200

    subscribe to disctinct events

--- config/events.rb
+++ config/events.rb
@@ -3,32 +3,50 @@
 Adhearsion::Events.draw do
 
   # punchblock do |event|
+  #   puts event
   # end
 
-  # Common events:
-  #   New|DTMF|PeerStatus|Originate|Masquerade|Rename|Bridge|Hangup|SoftHangup
-  #
+  ami name: 'Bridge' do |event|
+    AMQPManager.publish(event)
+  end
+
+  ami name: 'BridgeExec' do |event|
+    AMQPManager.publish(event)
+  end
+
   ami name: 'PeerStatus' do |event|
     AMQPManager.publish(event)
   end
 
-  ami name: 'DTMF' do |event|
+  ami name: 'NewCallerid' do |event|
+    AMQPManager.publish(event)
+  end
+
+  ami name: 'OriginateResponse' do |event|
+    AMQPManager.publish(event)
+  end
+
+  ami name: 'Newchannel' do |event|
+    AMQPManager.publish(event)
+  end
+
+  ami name: 'Newstate' do |event|
+    AMQPManager.publish(event)
+  end
+
+  ami name: 'Newexten' do |event|
+    AMQPManager.publish(event)
+  end
+
+  ami name: 'Newchannel' do |event|
+    AMQPManager.publish(event)
+  end
+
+  ami name: 'SoftHangupRequest' do |event|
     AMQPManager.publish(event)
   end
 
   ami name: 'Hangup' do |event|
-    AMQPManager.publish(event)
-  end
-
-  ami name: 'SoftHangup' do |event|
-    AMQPManager.publish(event)
-  end
-
-  ami name: 'Hangup' do |event|
-    AMQPManager.publish(event)
-  end
-
-  ami name: 'Bridge' do |event|
     AMQPManager.publish(event)
   end
 end
