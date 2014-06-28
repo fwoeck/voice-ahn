637b257d677a7894960011c2a988c9cea1902b49
Frank Woeckener <fwoeck@gmail.com>
2014-06-28 10:59:19 +0200

    use current ahn dev branches

--- config/events.rb
+++ config/events.rb
@@ -2,12 +2,33 @@
 
 Adhearsion::Events.draw do
 
-  punchblock do |event|
-    AMQPManager.push_publish(event.to_json)
-    AMQPManager.ahn_publish(event.to_json, routing_key: 'voice.ahn')
+  # punchblock do |event|
+  # end
+
+  # Common events:
+  #   New|DTMF|PeerStatus|Originate|Masquerade|Rename|Bridge|Hangup|SoftHangup
+  #
+  ami name: 'PeerStatus' do |event|
+    AMQPManager.publish(event)
   end
 
-  # ami name: 'PeerStatus' do |event|
-  #   puts event
-  # end
+  ami name: 'DTMF' do |event|
+    AMQPManager.publish(event)
+  end
+
+  ami name: 'Hangup' do |event|
+    AMQPManager.publish(event)
+  end
+
+  ami name: 'SoftHangup' do |event|
+    AMQPManager.publish(event)
+  end
+
+  ami name: 'Hangup' do |event|
+    AMQPManager.publish(event)
+  end
+
+  ami name: 'Bridge' do |event|
+    AMQPManager.publish(event)
+  end
 end
--- lib/amqp_manager.rb
+++ lib/amqp_manager.rb
@@ -1,14 +1,17 @@
 module AMQPManager
 
+
   def self.ahn_channel
     Thread.current[:ahn_channel] ||= @connection.create_channel
   end
+
 
   def self.ahn_xchange
     Thread.current[:ahn_xchange] ||= ahn_channel.topic('voice.ahn', auto_delete: false)
   end
 
-  def self.ahn_publish(*args)
+
+  def self.publish_to_ahn(*args)
     ahn_xchange.publish(*args)
   end
 
@@ -17,18 +20,28 @@ module AMQPManager
     Thread.current[:push_channel] ||= @connection.create_channel
   end
 
+
   def self.push_xchange
     Thread.current[:push_xchange] ||= push_channel.fanout('voice.push', auto_delete: false)
   end
 
-  def self.push_publish(*args)
+
+  def self.publish_to_push(*args)
     push_xchange.publish(*args)
+  end
+
+
+  def self.publish(event)
+    json_evt = event.to_json
+    publish_to_ahn(json_evt, routing_key: 'voice.ahn')
+    publish_to_push(json_evt)
   end
 
 
   def self.shutdown
     @connection.close
   end
+
 
   def self.start
     @connection = Bunny.new(
