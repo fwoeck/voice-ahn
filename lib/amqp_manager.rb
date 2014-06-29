005681f5cfec61db2aecd415cb9b5e50088d0d86
Frank Woeckener <fwoeck@gmail.com>
2014-06-29 09:09:20 +0200

    restructure ami-event messaging flow

--- lib/amqp_manager.rb
+++ lib/amqp_manager.rb
@@ -1,57 +1,37 @@
 module AMQPManager
+  class << self
 
 
-  def self.numbers_channel
-    Thread.current[:numbers_channel] ||= @connection.create_channel
-  end
+    def numbers_channel
+      Thread.current[:numbers_channel] ||= @connection.create_channel
+    end
 
 
-  def self.numbers_xchange
-    Thread.current[:numbers_xchange] ||= numbers_channel.topic('voice.numbers', auto_delete: false)
-  end
+    def numbers_xchange
+      Thread.current[:numbers_xchange] ||= numbers_channel.topic('voice.numbers', auto_delete: false)
+    end
 
 
-  def self.publish_to_numbers(*args)
-    numbers_xchange.publish(*args)
-  end
+    def numbers_publish(payload)
+      numbers_xchange.publish(payload.to_json, routing_key: 'voice.numbers')
+    end
 
 
-  def self.push_channel
-    Thread.current[:push_channel] ||= @connection.create_channel
-  end
+    def shutdown
+      @connection.close
+    end
 
 
-  def self.push_xchange
-    Thread.current[:push_xchange] ||= push_channel.fanout('voice.push', auto_delete: false)
-  end
+    def start
+      @connection = Bunny.new(
+        host:     AhnConfig['rabbit_host'],
+        user:     AhnConfig['rabbit_user'],
+        password: AhnConfig['rabbit_pass']
+      )
+      @connection.start
 
-
-  def self.publish_to_push(*args)
-    push_xchange.publish(*args)
-  end
-
-
-  def self.publish(event)
-    json_evt = event.to_json
-    publish_to_numbers(json_evt, routing_key: 'voice.numbers')
-    publish_to_push(json_evt)
-  end
-
-
-  def self.shutdown
-    @connection.close
-  end
-
-
-  def self.start
-    @connection = Bunny.new(
-      host:     AhnConfig['rabbit_host'],
-      user:     AhnConfig['rabbit_user'],
-      password: AhnConfig['rabbit_pass']
-    )
-    @connection.start
-
-    numbers_channel.queue('voice.numbers', auto_delete: false)
-                   .bind(numbers_xchange, routing_key: 'voice.numbers')
+      numbers_channel.queue('voice.numbers', auto_delete: false)
+                     .bind(numbers_xchange, routing_key: 'voice.numbers')
+    end
   end
 end
