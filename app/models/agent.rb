553222d62ec79ee6f45703a5e0d6ff8f64d2c9d9
Frank Wöckener <fwoeck@gmail.com>
2014-07-04 14:25:42 +0200

    refactor agent state updates

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -6,17 +6,31 @@ class Agent
   # TODO
   # Agent.where(availability: 'ready', languages: 'en').first => order FIFO/heuristic
 
+  class << self
+    def update_agent_state_with(payload)
+      uid, key, value = parse_payload(payload)
 
-  def self.update_agent_state_with(payload)
-    data  = JSON.parse payload
-    uid   = data.delete('user_id').to_i
-    key   = data.keys.first
-    value = data[key]
+      if uid > 0 && key
+        setter = "#{key}#{key[/y\z/] ? '' : 's'}="
+        Registry[uid].send(setter, value)
+        Adhearsion.logger.info "Update #{uid}'s state: #{setter}'#{value}'"
+      end
+    end
 
-    if uid > 0 && key
-      setter = "#{key}#{key[/y\z/] ? '' : 's'}="
-      Registry[uid].send(setter, value)
-      puts ">>> Update #{uid} status: #{setter}'#{value}'"
+    def parse_payload(payload)
+      data  = JSON.parse(payload)
+      assert data.keys.include?('user_id')
+      assert data.keys.count == 2
+
+      uid   = data.delete('user_id').to_i
+      key   = data.keys.first
+      value = data[key]
+
+      [uid, key, value]
+    end
+
+    def assert(value)
+      raise "An assertion failed!" unless !!value
     end
   end
 end
