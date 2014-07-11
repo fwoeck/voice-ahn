f30dcb357095843da19ee5ae6250a4c728e7a690
Frank Wöckener <fwoeck@gmail.com>
2014-07-11 06:05:34 +0200

    require agent excplicitly

--- app/models/array.rb
+++ app/models/array.rb
@@ -1,8 +1,10 @@
+require './app/models/agent'
+
 class Array
 
   def sort_by_idle_time
     self.sort { |a1, a2|
-      Registry[a1].idle_since <=> Registry[a2].idle_since
+      Agent::Registry[a1].idle_since <=> Agent::Registry[a2].idle_since
     }
   end
 end
