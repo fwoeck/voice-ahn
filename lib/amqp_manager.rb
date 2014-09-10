5cd7ca6a89df5ee0e7975d60403f4e46c3ba615f
Frank Wöckener <fwoeck@gmail.com>
2014-09-10 17:46:10 +0200

    close amqp channels after threaded sending

--- lib/amqp_manager.rb
+++ lib/amqp_manager.rb
@@ -2,60 +2,51 @@ require './app/models/agent'
 
 
 module AmqpManager
+  TOPICS = [:rails, :numbers, :custom, :ahn]
 
   class << self
 
-    def rails_channel
-      Thread.current[:rails_channel] ||= @connection.create_channel
-    end
-
-    def rails_xchange
-      Thread.current[:rails_xchange] ||= rails_channel.topic('voice.rails', auto_delete: false)
+    def close_channels
+      TOPICS.each { |name| Thread.current["#{name}_channel".to_sym].try(:close) }
     end
 
 
-    def custom_channel
-      Thread.current[:custom_channel] ||= @connection.create_channel
-    end
+    TOPICS.each { |name|
+      define_method "#{name}_channel" do
+        Thread.current["#{name}_channel".to_sym] ||= connection.create_channel
+      end
 
-    def custom_xchange
-      Thread.current[:custom_xchange] ||= custom_channel.topic('voice.custom', auto_delete: false)
-    end
+      define_method "#{name}_xchange" do
+        Thread.current["#{name}_xchange".to_sym] ||= send("#{name}_channel").topic("voice.#{name}", auto_delete: false)
+      end
 
-
-    def numbers_channel
-      Thread.current[:numbers_channel] ||= @connection.create_channel
-    end
-
-    def numbers_xchange
-      Thread.current[:numbers_xchange] ||= numbers_channel.topic('voice.numbers', auto_delete: false)
-    end
+      define_method "#{name}_queue" do
+        Thread.current["#{name}_queue".to_sym] ||= send("#{name}_channel").queue("voice.#{name}", auto_delete: false)
+      end
+    }
 
 
     def publish(payload, include_custom, include_numbers)
-      data = Marshal.dump(payload)
+      Thread.new {
+        data = Marshal.dump(payload)
 
-      rails_xchange.publish(data,   routing_key: 'voice.rails')
-      custom_xchange.publish(data,  routing_key: 'voice.custom')  if include_custom
-      numbers_xchange.publish(data, routing_key: 'voice.numbers') if include_numbers
+        rails_xchange.publish(data,   routing_key: 'voice.rails')
+        custom_xchange.publish(data,  routing_key: 'voice.custom')  if include_custom
+        numbers_xchange.publish(data, routing_key: 'voice.numbers') if include_numbers
+
+        close_channels
+      }
     end
 
 
-    def ahn_channel
-      Thread.current[:ahn_channel] ||= @connection.create_channel
-    end
-
-    def ahn_xchange
-      Thread.current[:ahn_xchange] ||= ahn_channel.topic('voice.ahn', auto_delete: false)
-    end
-
-    def ahn_queue
-      Thread.current[:ahn_queue] ||= ahn_channel.queue('voice.ahn', auto_delete: false)
+    def connection
+      establish_connection unless @connection
+      @connection
     end
 
 
     def shutdown!
-      @connection.close
+      connection.close
     end
 
 
